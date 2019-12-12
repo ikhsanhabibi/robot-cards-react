172caf2ee30611bfba282762bfecb512b3bfc227
Ikhsan Habibi <ikhsanhabibi48@gmail.com>
2019-12-12 16:00:28 +0100

    Updates

---
+++ precache-manifest.fee3ec7deb78bc7539df08e5de915938.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d4f6628b6a35699e4c5a2677a1f243e",
+    "url": "/robot-cards-react/index.html"
+  },
+  {
+    "revision": "6df1e9c555a179ee30b4",
+    "url": "/robot-cards-react/static/css/main.7e3ce1d1.chunk.css"
+  },
+  {
+    "revision": "b5ace48809d176a1533f",
+    "url": "/robot-cards-react/static/js/2.fcd9027c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/robot-cards-react/static/js/2.fcd9027c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6df1e9c555a179ee30b4",
+    "url": "/robot-cards-react/static/js/main.dd1a706d.chunk.js"
+  },
+  {
+    "revision": "fe3b8097dfba65f18276",
+    "url": "/robot-cards-react/static/js/runtime-main.b41435ac.js"
+  }
+]);
